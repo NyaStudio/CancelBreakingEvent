93b54114aabe9a2dad2f398c3e68b3811fd76de1
BLxcwg666 <huixcwg@gmail.com>
2025-06-20 02:37:55 +0800

    feat: add player interaction cancellation to prevent block actions

--- src/main/java/cn/nekopixel/cancelbreakingevent/CancelBreakingEvent.java
+++ src/main/java/cn/nekopixel/cancelbreakingevent/CancelBreakingEvent.java
@@ -2,8 +2,10 @@ package cn.nekopixel.cancelbreakingevent;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CancelBreakingEvent extends JavaPlugin implements Listener {
@@ -27,5 +29,13 @@ public class CancelBreakingEvent extends JavaPlugin implements Listener {
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
         event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Action action = event.getAction();
+        if (action == Action.LEFT_CLICK_BLOCK || action == Action.LEFT_CLICK_AIR) {
+            event.setCancelled(true);
+        }
     }
 }
